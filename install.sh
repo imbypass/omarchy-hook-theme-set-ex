b73686d9870e6a2af63b1bf700dc1b40109da92b
imbypass <coleschaefer1@gmail.com>
2025-10-26 14:52:29 -0400

    oops. really added the install script this time.

---
+++ install.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+rm -rf /tmp/theme-hook/
+
+git clone https://github.com/imbypass/omarchy-hook-theme-set-ex.git /tmp/theme-hook
+
+mv /tmp/theme-hook/theme-set ~/.config/omarchy/hooks/
+
+mkdir -p ~/.config/omarchy/hooks/theme-set.conf/
+mv /tmp/theme-hook/theme-set.conf/* ~/.config/omarchy/hooks/theme-set.conf/
+
+rm -rf /tmp/theme-hook
+
+chmod +x ~/.config/omarchy/hooks/theme-set
+chmod +x ~/.config/omarchy/hooks/theme-set.conf/*
+
+omarchy-theme-set $(omarchy-theme-current)
