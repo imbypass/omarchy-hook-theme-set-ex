c2e89dc04a1e1f4b8904614f8bfd2d689149a0e3
imbypass <coleschaefer1@gmail.com>
2025-10-31 18:28:16 -0400

    moved color parsing logic to theme-set script

--- recipes.d/10-walker.sh
+++ recipes.d/10-walker.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-colors_file="$HOME/.config/omarchy/current/theme/colors.json"
 output_file="$HOME/.config/omarchy/current/theme/walker.css"
-
-clean_color() {
-    echo "$1" | sed "s/['\"]//g" | sed 's/#//g' | sed 's/0x//g' | sed 's/0X//g'
-}
-
-colors=(black red green yellow blue magenta cyan white)
-declare -A normal_colors bright_colors
-
-primary_background=$(clean_color "$(jq -r '.primary.background' $colors_file)")
-primary_foreground=$(clean_color "$(jq -r '.primary.foreground' $colors_file)")
-for color in "${colors[@]}"; do
-    normal_colors[$color]=$(clean_color "$(jq -r ".normal.$color" $colors_file)")
-    bright_colors[$color]=$(clean_color "$(jq -r ".bright.$color" $colors_file)")
-done
-font=$(omarchy-font-current)
 
 if [[ ! -f "$output_file" ]]; then
     cat > "$output_file" << EOF
