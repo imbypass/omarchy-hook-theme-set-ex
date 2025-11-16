eb5ff759ae6829fc6580a2188e46f892842a1d5e
imbypass <coleschaefer1@gmail.com>
2025-11-16 15:59:08 -0500

    fixed steam theme dialogs being transparent

--- theme-set.d/40-steam.sh
+++ theme-set.d/40-steam.sh
@@ -13,26 +13,39 @@ fi
 if [[ ! -f "$output_file" ]]; then
     cat > "$output_file" << EOF
 :root {
+    /* The main accent color and the matching text value */
     --adw-accent-bg-rgb: ${rgb_normal_blue} !important;
     --adw-accent-fg-rgb: ${rgb_primary_background} !important;
     --adw-accent-rgb: ${rgb_normal_blue} !important;
+
+	/* destructive-action buttons */
     --adw-destructive-bg-rgb: ${rgb_normal_red} !important;
     --adw-destructive-fg-rgb: ${rgb_primary_foreground} !important;
     --adw-destructive-rgb: ${rgb_normal_red} !important;
+
+	/* Levelbars, entries, labels and infobars. These don't need text colors */
     --adw-success-bg-rgb: ${rgb_normal_green} !important;
     --adw-success-fg-rgb: ${rgb_normal_black} !important;
     --adw-success-rgb: ${rgb_normal_green} !important;
+
     --adw-warning-bg-rgb: ${rgb_bright_yellow} !important;
     --adw-warning-fg-rgb: ${rgb_primary_background} !important;
     --adw-warning-fg-a: 0.8 !important;
     --adw-warning-rgb: ${rgb_bright_yellow} !important;
+
     --adw-error-bg-rgb: ${rgb_normal_red} !important;
     --adw-error-fg-rgb: ${rgb_normal_black} !important;
     --adw-error-rgb: ${rgb_normal_red} !important;
+
+	/* Window */
     --adw-window-bg-rgb: ${rgb_primary_background} !important;
     --adw-window-fg-rgb: ${rgb_primary_foreground} !important;
+
+	/* Views - e.g. text view or tree view */
     --adw-view-bg-rgb: ${rgb_normal_black} !important;
     --adw-view-fg-rgb: ${rgb_primary_foreground} !important;
+
+	/* Header bar, search bar, tab bar */
     --adw-headerbar-bg-rgb: ${rgb_primary_background} !important;
     --adw-headerbar-fg-rgb: ${rgb_primary_foreground} !important;
     --adw-headerbar-border-rgb: ${rgb_bright_black} !important;
@@ -41,6 +54,8 @@ if [[ ! -f "$output_file" ]]; then
     --adw-headerbar-shade-a: 0.36 !important;
     --adw-headerbar-darker-shade-rgb: 0, 0, 0 !important;
     --adw-headerbar-darker-shade-a: 0.9 !important;
+
+	/* Split pane views */
     --adw-sidebar-bg-rgb: ${rgb_primary_background} !important;
     --adw-sidebar-fg-rgb: ${rgb_primary_foreground} !important;
     --adw-sidebar-backdrop-rgb: ${rgb_bright_black} !important;
@@ -51,17 +66,28 @@ if [[ ! -f "$output_file" ]]; then
     --adw-secondary-sidebar-backdrop-rgb: ${rgb_bright_black} !important;
     --adw-secondary-sidebar-shade-rgb: 0, 0, 0 !important;
     --adw-secondary-sidebar-shade-a: 0.36 !important;
+
+	/* Cards, boxed lists */
     --adw-card-bg-rgb: 255, 255, 255 !important;
     --adw-card-bg-a: 0.08 !important;
     --adw-card-fg-rgb: 255, 255, 255 !important;
     --adw-card-shade-rgb: 0, 0, 0 !important;
     --adw-card-shade-a: 0.36 !important;
-    --adw-dialog-bg-rgb: ${rgb_normal_black} !important;
+
+	/* Dialogs */
+    --adw-dialog-bg-rgb: ${rgb_primary_background} !important;
     --adw-dialog-fg-rgb: ${rgb_primary_foreground} !important;
-    --adw-popover-bg-rgb: ${rgb_normal_black} !important;
+
+    /* Popovers */
+    --adw-popover-bg-rgb: ${rgb_primary_background} !important;
     --adw-popover-fg-rgb: ${rgb_primary_foreground} !important;
-    --adw-thumbnail-bg-rgb: ${rgb_normal_black} !important;
-    --adw-popover-fg-rgb: ${rgb_primary_foreground} !important;
+    --adw-popover-shade-rgb: 0, 0, 0 !important;
+    --adw-popover-shade-a: 0.36 !important;
+
+	/* Thumbnails */
+    --adw-thumbnail-bg-rgb: ${rgb_primary_background} !important;
+
+	/* Miscellaneous */
     --adw-shade-rgb: 0, 0, 0 !important;
     --adw-shade-a: 0.36 !important;
 }
